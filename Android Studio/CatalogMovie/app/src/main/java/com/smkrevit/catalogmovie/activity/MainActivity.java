b65cbbd5ad0a8a966db06724ab2b0de0523bd97d
M-Faizhal <68377652+M-Faizhal@users.noreply.github.com>
2021-09-04 13:32:51 +0700

    ver 8.0

---
+++ Android Studio/CatalogMovie/app/src/main/java/com/smkrevit/catalogmovie/activity/MainActivity.java
@@ -0,0 +1,97 @@
+package com.smkrevit.catalogmovie.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
+
+import com.smkrevit.catalogmovie.R;
+import com.smkrevit.catalogmovie.adapter.MovieAdapter;
+import com.smkrevit.catalogmovie.model.Result;
+import com.smkrevit.catalogmovie.res.ApiClient;
+import com.smkrevit.catalogmovie.res.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MovieAdapter adapter;
+    private SearchView searchView;
+    String API_KEY = "a70699ecfc3c530f5f78171075d523f8";
+    String LANGUAGE = "en-US";
+    String CATEGORY = "popular";
+    int PAGE = 1;
+    RecyclerView recyclerView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rvMovie);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        CallRetrofit();
+    }
+
+    private void CallRetrofit() {
+        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+        Call<Response> call = apiInterface.getMovie(CATEGORY, API_KEY, LANGUAGE, PAGE);
+        call.enqueue(new Callback<Response>() {
+            @Override
+            public void onResponse(Call<Response> call, Response<Response> response) {
+                List<Result> mList = response.body().getResults();
+                adapter = new MovieAdapter(MainActivity.this, mList);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (newText.length() > 1) {
+                    ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+                    Call<Response> call = apiInterface.getQuery(API_KEY, LANGUAGE, newText, PAGE);
+                    call.enqueue(new Callback<Response>() {
+                        @Override
+                        public void onResponse(Call<Response> call, Response<Response> response) {
+                            List<Result> mList = response.body().getResults();
+                            adapter = new MovieAdapter(MainActivity.this, mList);
+                            recyclerView.setAdapter(adapter);
+                        }
+
+                        @Override
+                        public void onFailure(Call<Response> call, Throwable t) {
+
+                        }
+                    });
+                }
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
